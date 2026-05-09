ed618879ef25405f740937c9fd203bfd9b2d4313
Tran Dai DUong <trandaiduong27112001@gmail.com>
2026-05-10 02:04:55 +0700

    doi api

--- src/config.js
+++ src/config.js
@@ -2,4 +2,5 @@
  * Frontend Config
  * URL base cho Backend API
  */
-export const API_BASE = 'http://localhost:3001/api';
+// export const API_BASE = 'http://localhost:3001/api';
+export const API_BASE = 'https://ptit-quiz-hxgu.onrender.com/api';
